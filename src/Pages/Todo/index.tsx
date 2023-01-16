7d4a50b77e90cdc73db3be37c75f0aca14733f96
Youngwoo Park <parkyw1206@gmail.com>
2023-01-17 00:37:07 +0900

    feat: todo아이템 추가 기능 추가

--- src/Pages/Todo/index.tsx
+++ src/Pages/Todo/index.tsx
@@ -4,22 +4,33 @@ import { toast } from "react-toastify";
 import classnames from "classnames";
 import axios from "axios";
 import plusIcon from "../../shared/images/plus.png";
-import Modal from "react-modal";
 
 import "./index.scss";
 
+interface todoInterface {
+  title: string;
+  content: string;
+  id: string;
+  createdAt: string;
+  updatedAt: string;
+}
 function TodoPage() {
   const navigate = useNavigate();
   const [clickedContentIndex, changeClickedContentIndex] = useState(-1);
   const [isAddTodoModalOpen, toggleAddTodoModalOpen] = useState(false);
-  const [todoList, changeTodoList] = useState([]);
+  const [todoList, changeTodoList] = useState<todoInterface[]>([]);
+  const [newItem, setNewItem] = useState({ title: "", content: "" });
   useEffect(() => {
     if (!window.localStorage.getItem("token")) {
       toast.error("토큰이 없어 로그인화면으로 전환됩니다.");
       navigate("/auth");
     }
     axios
-      .get("http://localhost:8080/todos")
+      .get("http://localhost:8080/todos", {
+        headers: {
+          Authorization: window.localStorage.getItem("token"),
+        },
+      })
       .then(function (response) {
         const responseData = response.data;
         changeTodoList(responseData.data);
@@ -29,27 +40,102 @@ function TodoPage() {
         toast.error(errorResponse.details ?? "알 수 없는 오류");
       });
   }, []);
+  const handleInserNewTodoItem = () => {
+    axios
+      .post(
+        "http://localhost:8080/todos",
+        {
+          title: newItem.title,
+          content: newItem.content,
+        },
+        {
+          headers: {
+            Authorization: window.localStorage.getItem("token"),
+          },
+        }
+      )
+      .then(function (response) {
+        changeTodoList([...todoList, response.data.data]);
+        toggleAddTodoModalOpen(false);
+      })
+      .catch((error) => {
+        console.log(error.response);
+        const errorResponse = error.response.data;
+        toast.error(errorResponse.details ?? "알 수 없는 오류");
+      });
+  };
   return (
     <div className="todo">
       {isAddTodoModalOpen && (
-        <Modal isOpen={isAddTodoModalOpen}>
-          새 Todo 추가
-          <button onClick={() => toggleAddTodoModalOpen(false)}>close</button>
-        </Modal>
+        <div className={"todo-modal"}>
+          <div className={"todo-innerModal"}>
+            <div className={"header"}>
+              <span>새 할일 추가</span>
+              <span
+                onClick={() => {
+                  setNewItem({ title: "", content: "" });
+                  toggleAddTodoModalOpen(false);
+                }}
+                className={"close"}
+              >
+                X
+              </span>
+            </div>
+            <div className={"body"}>
+              <span>
+                제목
+                <input
+                  className="todo-innerModal--input"
+                  placeholder="제목을 입력하세요."
+                  onChange={(e) =>
+                    setNewItem({ ...newItem, title: e.target.value })
+                  }
+                />
+              </span>
+              <span>
+                내용
+                <input
+                  className="todo-innerModal--input"
+                  placeholder="내용을 입력하세요."
+                  onChange={(e) =>
+                    setNewItem({ ...newItem, content: e.target.value })
+                  }
+                />
+              </span>
+            </div>
+            <div className={"todo-footer"}>
+              <button
+                className={"todo-footer--add"}
+                onClick={() => handleInserNewTodoItem()}
+              >
+                추가
+              </button>
+              <button
+                className={"todo-footer--cancel "}
+                onClick={() => {
+                  toggleAddTodoModalOpen(false);
+                  setNewItem({ title: "", content: "" });
+                }}
+              >
+                취소
+              </button>
+            </div>
+          </div>
+        </div>
       )}
       <div className="todo-body">
         <span className="todo-todoTypo">Todo</span>
         <span className="todo-subBody">
           <div className="todo-left">
-            {todoList.map((item, index) => (
+            {todoList.map((item: todoInterface, index) => (
               <div
                 className={classnames("todo-item", {
                   clicked: clickedContentIndex === index,
                 })}
                 onClick={() => changeClickedContentIndex(index)}
               >
-                <span className="todo-number">{index}</span>
-                <span className="todo-contentName">{item}</span>
+                <span className="todo-number">{index + 1}</span>
+                <span className="todo-contentName">{item.title}</span>
               </div>
             ))}
             <div
